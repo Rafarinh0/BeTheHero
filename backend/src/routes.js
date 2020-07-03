7b61240f2e77d0d92fc07acb81ff2b30ec1c7a95
Rafael Marinho Gonçalves Ferreira <rafael.mgf2001@gmail.com>
2020-07-03 16:09:40 -0300

    Estabelecimento de conexão de rotas com o banco de dados

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,14 +1,31 @@
 const express = require('express');//puxando o express
+const crypto = require('crypto');
+
+const connection = require('./database/connection');
 
 const routes = express.Router();//setando o express
-routes.use(express.json());//setando possibilidade de usar json
 
-routes.get('/users', (req,res)=>{//rota
-    return res.json({
-        evento: 'festa do chá',
-        realizador: 'marocas'
-    })
+routes.get('/ongs', async (req, res) => {
+    const ongs = await connection('ongs').select('*');
 
+    return res.json(ongs);
 });
 
+routes.post('/ongs', async (req, res) => {//rota
+
+    const { name, email, whatsapp, city, uf } = req.body;
+
+    const id = crypto.randomBytes(4).toString('HEX');
+
+    await connection('ongs').insert({
+        id,
+        name,
+        email,
+        whatsapp,
+        city,
+        uf,
+    })
+
+    return res.json({ id });
+});
 module.exports = routes;
