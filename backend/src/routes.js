353c2fd5c34310671c4ff5571f0432a7f59e7f8a
Rafael Marinho Gonçalves Ferreira <rafael.mgf2001@gmail.com>
2020-07-03 17:10:58 -0300

    Rotas de listagem e criação de incidentes

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,10 +1,14 @@
 const express = require('express');//puxando o express
 
 const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
 
 const routes = express.Router();//setando o express
 
 routes.get('/ongs', OngController.index);
-
 routes.post('/ongs', OngController.create);
+
+routes.get('/incidents', IncidentController.index);
+routes.post('/incidents', IncidentController.create);   
+
 module.exports = routes;
