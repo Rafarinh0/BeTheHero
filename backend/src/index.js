285df3863ba04c30ced91103b1e8833c74c19a13
Rafael Marinho Gonçalves Ferreira <rafael.mgf2001@gmail.com>
2020-07-03 18:25:35 -0300

    Adição do CORS

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,10 @@
 const express = require('express');//puxando o express
+const cors = require('cors');
 const routes = require('./routes');//puxando o routes
 
 const app = express();//setando o express
 
+app.use(cors());
 app.use(express.json());//setando possibilidade de usar json
 app.use(routes);//setando o arquivo que contém as rotas
 
