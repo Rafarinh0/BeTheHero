fe54685bb393805bcff41106a195e96c108e2f7f
Rafael Marinho Gonçalves Ferreira <rafael.mgf2001@gmail.com>
2020-07-06 18:33:17 -0300

    Validação das rotas da aplicação

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,13 @@
 const express = require('express');//puxando o express
 const cors = require('cors');
 const routes = require('./routes');//puxando o routes
+const {errors} = require('celebrate');
 
 const app = express();//setando o express
 
 app.use(cors());
 app.use(express.json());//setando possibilidade de usar json
 app.use(routes);//setando o arquivo que contém as rotas
+app.use(errors());
 
 app.listen(3333); //colocar o servidor no ar
